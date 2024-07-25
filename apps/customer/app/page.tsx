6d8a656e72e40591719a29facad8bcc19183d015
Rachmizard <rachmizard11072000@gmail.com>
2024-07-25 12:29:12 +0700

    chore: setup prettier config & sort imports lib

--- apps/customer/app/page.tsx
+++ apps/customer/app/page.tsx
@@ -1,38 +1,35 @@
-import { Button } from "@repo/ui";
+import { Button, cn } from '@repo/ui';
 
 export default function Home() {
-  return (
-    <div
-      className="bg-neutral-50
-    flex
-    min-h-screen
-    flex-col
-    items-center
-    justify-center"
-    >
-      <div className="flex flex-col items-center justify-center">
-        <div className="text-center">
-          <h1 className="text-4xl font-bold">Shuffle</h1>
-          <p className="text-lg">
-            Shuffle is a web app that allows you to shuffle your music against
-            your friends.
-          </p>
-        </div>
-        <div className="mt-10 flex flex-col items-center justify-center">
-          <div className="flex flex-col items-center justify-center">
-            <div className="w-full">
-              <div className="relative">
-                <div className="absolute inset-0 flex items-center">
-                  <div className="w-full border-t border-gray-300" />
+    return (
+        <div
+            className={cn(
+                'flex min-h-screen flex-col items-center justify-center bg-neutral-50',
+            )}
+        >
+            <div className="flex flex-col items-center justify-center">
+                <div className="text-center">
+                    <h1 className="text-4xl font-bold">Shuffle</h1>
+                    <p className="text-lg">
+                        Shuffle is a web app that allows you to shuffle your
+                        music against your friends.
+                    </p>
                 </div>
-                <div className="relative flex justify-center text-sm">
-                  <Button>Sign in with Google</Button>
+                <div className="mt-10 flex flex-col items-center justify-center">
+                    <div className="flex flex-col items-center justify-center">
+                        <div className="w-full">
+                            <div className="relative">
+                                <div className="absolute inset-0 flex items-center">
+                                    <div className="w-full border-t border-gray-300" />
+                                </div>
+                                <div className="relative flex justify-center text-sm">
+                                    <Button>Sign in with Google</Button>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-              </div>
             </div>
-          </div>
         </div>
-      </div>
-    </div>
-  );
+    );
 }
---
+++ apps/customer/prettier.config.js
@@ -0,0 +1,1 @@
+module.exports = require("@repo/sort-imports-config/next");
