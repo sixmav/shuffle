ffe33edbd1f9b4b2139db07d1f0b9372e111253a
Ismail Rohaga <azh.zhafir@gmail.com>
2024-08-03 17:07:48 +0700

    feat: add Dockerfile and basic impl

--- apps/service/src/main.rs
+++ apps/service/src/main.rs
@@ -1,28 +1,39 @@
-use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
+use std::{env, io};
+
+use actix_cors::Cors;
+use actix_web::{get, http, App, HttpResponse, HttpServer, Responder};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv::dotenv().expect("Failed to read .env file");
+    env::set_var("RUST_LOG", "actix_web=debug");
+    env_logger::init();
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
+    let app_host = env::var("APP_HOST").expect("APP_HOST not found.");
+    let app_port = env::var("APP_PORT").expect("APP_PORT not found.");
+    let app_url = format!("{}:{}", &app_host, &app_port);
 
-#[actix_web::main]
-async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| {
+    HttpServer::new(move || {
         App::new()
+            .wrap(
+                Cors::default() // allowed_origin return access-control-allow-origin: * by default
+                    .allowed_origin("http://127.0.0.1:3000")
+                    .allowed_origin("http://localhost:3000")
+                    .send_wildcard()
+                    .allowed_methods(vec!["GET", "POST", "PUT", "DELETE"])
+                    .allowed_headers(vec![http::header::AUTHORIZATION, http::header::ACCEPT])
+                    .allowed_header(http::header::CONTENT_TYPE)
+                    .max_age(3600),
+            )
+            .wrap(actix_web::middleware::Logger::default())
             .service(hello)
-            .service(echo)
-            .route("/hey", web::get().to(manual_hello))
     })
-    .bind(("127.0.0.1", 8080))?
+    .bind(&app_url)?
     .run()
     .await
 }
